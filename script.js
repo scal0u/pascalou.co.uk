ff39936c1f67f05ba26949e79e5ffb93f4a0e558
Pascal Conges <pascal.conges@brewers.co.uk>
2016-02-24 17:27:18 +0000

    Hello world

---
+++ script.js
@@ -0,0 +1,12 @@
+setInterval(function() {
+
+	$("#logo").parent().toggleClass("blur").toggleClass("shifted");
+
+}, 2000);
+
+
+setInterval(function() {
+
+	$(".j1").toggleClass("messed_up");
+
+}, 5500);
