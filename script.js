1afd2c0314ff8497d02d34de7f15286c0b3e4283
Pascal Conges <pascal.conges@brewers.co.uk>
2016-02-25 10:49:03 +0000

    Enabled check out my work button

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ setInterval(function() {
 	$(".j1").toggleClass("messed_up");
 
 }, 10000);
+
+
+function checkOutMyWork() {
+	$('html, body').animate({
+        scrollTop: $(".j2").offset().top
+    }, 1000);
+}
