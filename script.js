21715791bad94da5f2ecab833111ac089a8f3cbc
Pascal Conges <pascal.conges@brewers.co.uk>
2016-02-25 12:19:59 +0000

    Added clouds :-3

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ setInterval(function() {
 
 
 setInterval(function() {
-
 	$(".j1").toggleClass("messed_up");
-
 }, 10000);
 
+function conveyorLoop() {
+	$('.j1').animate({
+		'background-position-y': '+=102'
+	}, 8000, 'linear', conveyorLoop);
+};
 
 function checkOutMyWork() {
 	$('html, body').animate({
